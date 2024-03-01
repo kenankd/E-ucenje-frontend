da6e8046b948c58136ce1a70260976cdee01b60f
kenankd <kdizdarevi1@etf.unsa.ba>
2024-03-01 23:44:19 +0100

    course dummy page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
