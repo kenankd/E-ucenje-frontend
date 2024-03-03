3a61ca7f8e2c57237a35bb3721a1dd444154705f
kenankd <kdizdarevi1@etf.unsa.ba>
2024-03-03 22:53:13 +0100

    routing and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { CoursesComponent } from './courses/courses.component';
 import { CourseCategoriesComponent } from './course-categories/course-categories.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
+
+const appRoutes : Routes = [
+  {
+    path: "home", component: CourseCategoriesComponent
+  },
+  {
+    path: "mycourses", component: CoursesComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     CoursesComponent,
-    CourseCategoriesComponent
+    CourseCategoriesComponent,
+    CourseDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
